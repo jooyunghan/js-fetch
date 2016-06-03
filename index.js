a7b953c4ee3ddd6f6adf87918829b0ffc016194f
Jooyung Han <jooyung.han@gmail.com>
2016-06-03 18:07:08 +0900

    Add Monad instance

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ Done.prototype.map = function map(f) {
   return new Done(f(this.a));
 }
 
+Done.prototype.flatMap = function flatMap(k) {
+  return k(this.a);
+}
+
 function Blocked(cont) {
   this.cont = cont;
 } 
@@ -20,7 +24,11 @@ Blocked.prototype = new Fetch();
 
 Blocked.prototype.map = function map(f) {
   return new Blocked(this.cont.map(f));
-} 
+}
+
+Blocked.prototype.flatMap = function flatMap(k) {
+  return new Blocked(this.cont.flatMap(k));
+}
 
 function interleave(tasks) {
   while (tasks.length > 0) {
@@ -37,7 +45,8 @@ function interleave(tasks) {
 function test() {
   const task1 = new Blocked(new Blocked(new Done(3)));
   const task2 = new Blocked(new Blocked(new Done(4)));
-  interleave([task1, task2]);
+  const task3 = task1.flatMap(a => task2.map(b => a + b));
+  interleave([task1, task2, task3]);
 }
 
 test();
